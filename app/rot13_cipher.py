29cae113096ef1f5f2167b7a7d73f54924f0740b
Jonhatan Morón Ojalvo <Jonidimoni23@hotmail.com>
2022-03-03 22:18:56 +0100

    cambio rot13

--- app/rot13_cipher.py
+++ app/rot13_cipher.py
@@ -1,49 +1,49 @@
 import string
 
 
-def cif_rot(str_in, shift):
-    ap = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+def cifrado_rot(str_in, shift):
+    alpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
     n = len(str_in)
     str_out = ""
 
     for i in range(n):
         c = str_in[i].upper()
-        l = ap.find(c)
-        newloc = (l + shift) % 26
-        str_out += ap[newloc]
+        loc = alpha.find(c)
+        newloc = (loc + shift) % 26
+        str_out += alpha[newloc]
 
     return str_out
 
 
-def cif_rot13(msg):
-    return cif_rot(msg, 13)
+def cifrado_rot13(msg):
+    return cifrado_rot(msg, 13)
 
 
-def cif_cesar(msg):
-    return cif_rot(msg, 3)
+def cifrado_cesar(msg):
+    return cifrado_rot(msg, 3)
 
 
-print(cif_cesar("foobar"))
-print(cif_rot("foobar"))
+print(cifrado_cesar("foobar"))
+print(cifrado_rot13("foobar"))
 
 
-def cif_rot_2(n):
-    lw = string.ascii_lowercase
-    up = string.ascii_uppercase
-    tr = str.maketrans(lw + up, lw[:n] + up[:n])
-    return lambda s: str.translate(s, tr)
+def cifrado_rot_2(n):
+    lc = string.ascii_lowercase
+    uc = string.ascii_uppercase
+    trans = str.maketrans(lc + uc, lc[:n] + uc[:n] + uc[:n])
+    return lambda s: str.translate(s, trans)
 
 
-def cif_cesar_2():
-    return cif_rot_2(3)
+def cifrado_cesar_2():
+    return cifrado_rot_2(3)
 
 
-def cif_rot13_2():
-    return cif_rot_2(13)
+def cifrado_rot13_2():
+    return cifrado_rot_2(13)
 
 
-cesar = cif_cesar_2()
-rot13 = cif_rot13_2()
+cesar = cifrado_cesar_2()
+rot13 = cifrado_rot13_2()
 
 print(cesar("footbar"))
 print(rot13("footbar"))

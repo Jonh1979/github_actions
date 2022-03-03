6bc6c5d240b0d128beeae0a8bfd0139fd51940dd
Jonhatan Morón <jonidimoni23@hotmail.com>
2022-03-03 20:31:42 +0100

    cambios rot13

--- tests/test_rot13_cipher.py
+++ tests/test_rot13_cipher.py
@@ -4,7 +4,7 @@ from app.operaciones import rot13_cipher
 
 class TestClass:
     def test_rot13_cipher(self):
-        assert rot13_cipher("footbar") == "BASCDA"
-        assert rot13_cipher("HELLO WORLD") == "AFDAF"
+        assert rot13_cipher("footbar") == "fafdasaf"
+        assert rot13_cipher("HELLO WORLD") == "fsafdsaf"
         assert rot13_cipher("HolaMundo") == "hafjakfjdsa"
 
